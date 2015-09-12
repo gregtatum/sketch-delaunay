adff2757049e72be6ffa4db186e4a1da37e0c8c0
Greg Tatum <tatum.creative@gmail.com>
2015-09-11 20:29:40 -0500

    Delaunay triangulation

--- triangle.js
+++ triangle.js
@@ -1,3 +1,4 @@
+var CreateTriangles = require('delaunay-triangulate')
 
 function _prepCanvasAndGetCtx() {
 	
@@ -9,7 +10,8 @@ function _prepCanvasAndGetCtx() {
 	return canvas.getContext('2d')
 }
 
-function _clickToCreatePoints( points, draw ) {
+function _clickToCreatePoints( current, draw ) {
+	
 	
 	$('canvas').click(function(e) {
 		var x = e.pageX * devicePixelRatio
@@ -17,44 +19,95 @@ function _clickToCreatePoints( points, draw ) {
 		
 		var point = [x,y]
 		
-		points.push(point)
+		current.points.push(point)
+		current.triangles = CreateTriangles( current.points )
+		
+		console.log(current.triangles)
 		draw()
 	})
 }
 
-function _drawFn( ctx, config, points ) {
+function _drawTriangles( ctx, config, triangles, points ) {
+
+	ctx.lineWidth = config.lineWidth * devicePixelRatio
+	ctx.strokeStyle = config.lineColor
+
+	ctx.beginPath()
+	
+	triangles.forEach(function( triangle ) {
+		
+		console.log( 'drawing triangle', triangle )
+		
+		var pt1 = points[triangle[0]]
+		var pt2 = points[triangle[1]]
+		var pt3 = points[triangle[2]]
+		
+		ctx.moveTo( pt1[0], pt1[1] )
+		ctx.lineTo( pt2[0], pt2[1] )
+
+		ctx.moveTo( pt2[0], pt2[1] )
+		ctx.lineTo( pt3[0], pt3[1] )
+
+		ctx.moveTo( pt3[0], pt3[1] )
+		ctx.lineTo( pt1[0], pt1[1] )
+	})
+	
+	ctx.stroke()
+	ctx.closePath()
+	
+	
+}
+
+function _drawPoints( ctx, config, points ) {
 	
 	var size = config.pointSize * devicePixelRatio
 	var halfSize = config.pointSize / 2 * devicePixelRatio
 	
+	ctx.fillStyle = config.pointColor
+
+	points.forEach(function( pt ) {
+		
+		ctx.fillRect(
+			pt[0] - halfSize,
+			pt[1] - halfSize,
+			size,
+			size
+		)
+	})
+}
+
+function _drawFn( ctx, config, current ) {
+	
 	return function draw() {
 		
-		ctx.fillStyle = "#fff"
-
-		points.forEach(function( pt ) {
-			
-			ctx.fillRect(
-				pt[0] - halfSize,
-				pt[1] - halfSize,
-				size,
-				size
-			)
-		})
+		ctx.clearRect(
+			0, 0,
+			window.innerWidth * devicePixelRatio,
+			window.innerHeight * devicePixelRatio
+		)
+		
+		_drawTriangles( ctx, config, current.triangles, current.points )
+		_drawPoints( ctx, config, current.points )
 	}
 }
 
 function init() {
 	
 	var ctx = _prepCanvasAndGetCtx()
-	var points = []
-	
+	var current = {
+		points : [],
+		triangles : []
+	}
 	var config = {
-		pointSize : 4
+		pointSize : 4,
+		pointColor : "#fff",
+		lineWidth : 2,
+		lineColor : "#03e"
 	}
 	
-	var draw = _drawFn( ctx, config, points )
+	var draw = _drawFn( ctx, config, current )
 	
-	_clickToCreatePoints( points, draw )
+	_clickToCreatePoints( current, draw )
 }
 
 jQuery(init)
